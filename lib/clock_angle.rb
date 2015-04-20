754eebf3fae404121d92ffb2e3900177bd8610b6
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 14:14:15 -0700

    Write and pass last test.

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -7,6 +7,11 @@ class ClockTime
   define_method(:clock_angle) do
     hour_angle = (@hours % 12)*30+(@minutes / 2)
     minutes_angle = @minutes * 6
-    angle_difference = (hour_angle - minutes_angle).abs    
+    angle_difference = (hour_angle - minutes_angle).abs
+    if angle_difference > 180
+      360 - angle_difference
+    else
+      angle_difference
+    end
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,7 +3,7 @@ require('clock_angle')
 
 describe('ClockTime') do
   describe('ClockTime#clock_angle') do
-    it('is 0 at 12 oclock') do
+    it('calculates the angle distance between minute and hour hands, e.g. is 0 at 12 oclock') do
       test_case = ClockTime.new(12,0)
       expect(test_case.clock_angle()).to(eq(0))
     end
@@ -13,14 +13,14 @@ describe('ClockTime') do
       expect(test_case.clock_angle()).to(eq(180))
     end
 
-    it('is 195 at 6:30') do
+    it('is 15 at 6:30') do
       test_case = ClockTime.new(6,30)
       expect(test_case.clock_angle()).to(eq(15))
     end
 
     it('picks smaller of the two angles when it is 85 at 9:10') do
       test_case = ClockTime.new(9,10)
-      expect(test_case.clock_angle()).to(eq(85))
+      expect(test_case.clock_angle()).to(eq(145))
     end
   end
 
