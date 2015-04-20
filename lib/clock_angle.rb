e226e305751bd19fb7f2fc29995763c2921bcbcc
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 13:59:09 -0700

    Write fourth test, write and pass third test.

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -4,7 +4,9 @@ class ClockTime
     @minutes = minutes
   end
 
-  define_method(:clock_angle) do 
-    0
+  define_method(:clock_angle) do
+    hour_angle = (@hours % 12)*30+(@minutes / 2)
+    minutes_angle = @minutes * 6
+    angle_difference = (hour_angle - minutes_angle).abs    
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -7,5 +7,21 @@ describe('ClockTime') do
       test_case = ClockTime.new(12,0)
       expect(test_case.clock_angle()).to(eq(0))
     end
+
+    it('is 180 at 6 oclock') do
+      test_case = ClockTime.new(6,0)
+      expect(test_case.clock_angle()).to(eq(180))
+    end
+
+    it('is 195 at 6:30') do
+      test_case = ClockTime.new(6,30)
+      expect(test_case.clock_angle()).to(eq(15))
+    end
+
+    it('picks smaller of the two angles when it is 85 at 9:10') do
+      test_case = ClockTime.new(9,10)
+      expect(test_case.clock_angle()).to(eq(85))
+    end
   end
+
 end
